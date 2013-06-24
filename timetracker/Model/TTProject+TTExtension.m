972470f18598929aab5dd725e57b362c205d297e
Florian Goessler <mail@floriangoessler.de>
2013-06-24 18:14:19 +0200

    implemented start/stop tracking

--- timetracker/Model/TTProject+TTExtension.m
+++ timetracker/Model/TTProject+TTExtension.m
@@ -7,7 +7,21 @@
 //
 
 #import "TTProject+TTExtension.h"
+#import "TTIssue+TTExtension.h"
+#import "TTLogEntries+TTExtension.h"
+
 
 @implementation TTProject (TTExtension)
+
+-(TTIssue*)currentIssue {
+	TTIssue *mostCurrentIssue = self.defaultIssue;
+	for (TTIssue *issue in self.childIssues) {
+		if([mostCurrentIssue.latestLogEntry.startDate compare:issue.latestLogEntry.startDate] == NSOrderedAscending) {
+			mostCurrentIssue = issue;
+		}
+	}
+	
+	return mostCurrentIssue;
+}
 
 @end
--- timetracker/ViewController/TTTrackingVC.m
+++ timetracker/ViewController/TTTrackingVC.m
@@ -7,42 +7,70 @@
 //
 
 #import "TTTrackingVC.h"
+#import "TTIssue+TTExtension.h"
+#import "TTLogEntries+TTExtension.h"
+
 
 @interface TTTrackingVC ()
 @property (weak, nonatomic) IBOutlet UILabel *timeLbl;
 @property (weak, nonatomic) IBOutlet UILabel *currentIssueLbl;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIButton *trackingBtn;
 
+@property (strong, nonatomic) NSTimer *pollingTimer;
 @end
 
 @implementation TTTrackingVC
+
 - (IBAction)changeIssueBtnClicked:(id)sender {
 }
 
 - (IBAction)trackingBtnClicked:(id)sender {
+	NSError *err = nil;
+	if(self.project.currentIssue.latestLogEntry != nil && self.project.currentIssue.latestLogEntry.endDate == nil) {
+		//stop tracking if still running
+		[self.project.currentIssue stopTracking:&err];
+	} else {
+		//start new tracking otherwise
+		[self.project.currentIssue startTracking:&err];
+	}
+
+	if(err != nil) {
+		NSLog(@"FEHLER! %@", err);
+	}
+	
+	[self updateViews];
 }
+
 - (IBAction)moreInfoBtnClicked:(id)sender {
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+-(void)updateViews {
+	TTIssue *currentIssue = self.project.currentIssue;
+	
+	self.currentIssueLbl.text = [NSString stringWithFormat:@"Current Issue: %@", currentIssue.name];
+		
+	self.timeLbl.text = [NSString stringWithNSTimeInterval:currentIssue.latestLogEntry.timeInterval];
+		
+	if(currentIssue.latestLogEntry != nil && currentIssue.latestLogEntry.endDate == nil) {
+		[self.trackingBtn setTitle:@"Stop Tracking" forState:UIControlStateNormal];
+		self.pollingTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateViews) userInfo:nil repeats:YES];
+	} else {
+		[self.pollingTimer invalidate];
+		[self.trackingBtn setTitle:@"Start Tracking" forState:UIControlStateNormal];
+		self.timeLbl.text = @"00:00:00";
+	}
 }
 
-- (void)viewDidLoad
+- (void)viewWillAppear:(BOOL)animated
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    [super viewWillAppear:animated];
+	
+	[self updateViews];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewWillDisappear:(BOOL)animated {
+	[self.pollingTimer invalidate];
 }
 
 @end
